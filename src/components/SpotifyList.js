e8426ddef4302d7e80c41d94fc89048a04162a36
unartcs <unartcs@gmail.com>
2023-04-26 00:51:34 +0200

    Fixed the Podcast API
    Hopefully the API is fixed, switched to try and catch method

--- src/components/SpotifyList.js
+++ src/components/SpotifyList.js
@@ -4,23 +4,28 @@ import podcastSpotifyData from "../data/PodcastData";
 function SpotifyList() {
   const [accessToken, setAccessToken] = useState("");
   const [episodeList, setEpisodeList] = useState({});
-  const [enableView, setEnableView] = useState(false)
+  const [enableView, setEnableView] = useState(false);
   const didMount = useRef(false);
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
   const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
   const PODCAST_ID = "6iqBa4nNBqoinj5y44yrlE";
   useEffect(() => {
-    setEpisodeList(['']);
+    setEpisodeList("");
     updateStuff();
   }, []);
   useEffect(() => {
     setTimeout(() => {
       searchShows();
-      setTimeout(() => {
-        setEnableView(true)
-      }, 200)
-    }, 1000);
+    }, 100);
   }, [accessToken]);
+  useEffect(() => {
+    if (episodeList !== "") {
+      setTimeout(() => {
+        // console.log(episodeList);
+        // setEnableView(true);
+      }, 2000);
+    }
+  }, [episodeList]);
   async function updateStuff() {
     var authParemeters = {
       method: "POST",
@@ -29,15 +34,38 @@ function SpotifyList() {
       },
       body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
     };
-    await fetch("https://accounts.spotify.com/api/token", authParemeters)
-      .then((result) => result.json())
-      .then((data) => setAccessToken(data.access_token));
+    // await fetch("https://accounts.spotify.com/api/token", authParemeters)
+    //   .then((result) => result.json())
+    //   .then((data) => setAccessToken(data.access_token));
+
+    // const api = await fetch(
+    //   "https://accounts.spotify.com/api/token",
+    //   authParemeters
+    // );
+    // const data = await api.json();
+    // setAccessToken(data.access_token);
+    let api = "";
+    try {
+      api = await fetch(
+        "https://accounts.spotify.com/api/token",
+        authParemeters
+      );
+    } catch (error) {
+      console.log("There was an error", error);
+    }
+    if (api?.ok) {
+      console.log("Use the response here!");
+      const data = await api.json();
+      setAccessToken(data.access_token);
+    } else {
+      console.log(`HTTP Response Code: ${api?.status}`);
+    }
   }
   async function searchShows() {
-    var auth = {
+    let auth = {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        authorization: `Bearer ` + accessToken,
         "Content-Type": "application/json",
       },
     };
@@ -45,17 +73,47 @@ function SpotifyList() {
     //   `https://api.spotify.com/v1/shows/6iqBa4nNBqoinj5y44yrlE/episodes?offset=0&limit=20`,
     //   authParemeters
     // );
-    let show = await fetch(
-      "https://api.spotify.com/v1/shows/6iqBa4nNBqoinj5y44yrlE?market=US",
-      auth
-    )
-      .then((response) => response.json())
-      .then((data) => setEpisodeList(data));
+    // let show = await fetch(
+    //   "https://api.spotify.com/v1/shows/6iqBa4nNBqoinj5y44yrlE?market=US",
+    //   auth
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => setEpisodeList(data));
+
+    // let api = await fetch(
+    //   "https://api.spotify.com/v1/shows/6iqBa4nNBqoinj5y44yrlE?market=US",
+    //   auth
+    // );
+    // let data = await api.json();
+    // setEpisodeList(await data);
+    let api = "";
+    try {
+      api = await fetch(
+        "https://api.spotify.com/v1/shows/6iqBa4nNBqoinj5y44yrlE?market=US",
+        auth
+      );
+    } catch (error) {
+      console.log("There was an error", error);
+    }
+    if (api?.ok) {
+      console.log("Use the response here!");
+      const data = await api.json();
+      setEpisodeList(data);
+      setEnableView(true);
+    } else {
+      console.log(`HTTP Response Code: ${api?.status}`);
+    }
   }
   return (
     <div className="spotify-list-wrapper">
       {enableView ? (
-        <div className={enableView ? "podcast-grid-container" : 'podcast-grid-container hide'}>
+        <div
+          className={
+            enableView
+              ? "podcast-grid-container"
+              : "podcast-grid-container hide"
+          }
+        >
           {episodeList.episodes.items.map((x) => {
             return (
               <iframe
@@ -73,22 +131,29 @@ function SpotifyList() {
           })}
         </div>
       ) : (
-        <div key={Math.random()} className={enableView ? "podcast-grid-container hide" : 'podcast-grid-container'}>
-        {podcastSpotifyData.map((item) => {
-          return (
-            <iframe
-              style={{ borderRadius: "15px" }}
-              src={item.link}
-              width="100%"
-              height="152"
-              frameBorder="0"
-              allowfullscreen=""
-              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
-              loading="lazy"
-              key={`${Math.floor(Math.random())}`}
-            ></iframe>
-          );
-        })}
+        <div
+          key={Math.random()}
+          className={
+            enableView
+              ? "podcast-grid-container hide"
+              : "podcast-grid-container"
+          }
+        >
+          {podcastSpotifyData.map((item) => {
+            return (
+              <iframe
+                style={{ borderRadius: "15px" }}
+                src={item.link}
+                width="100%"
+                height="152"
+                frameBorder="0"
+                allowfullscreen=""
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
+                loading="lazy"
+                key={`${Math.floor(Math.random())}`}
+              ></iframe>
+            );
+          })}
         </div>
       )}
     </div>
