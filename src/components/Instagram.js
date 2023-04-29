de4de2a218fac78bd09e6f79d6f7d65fb7d6829d
unartcs <unartcs@gmail.com>
2023-04-29 13:08:32 +0200

    Changed tokens

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -15,7 +15,7 @@ function Instagram() {
   // }
   const fetchData = async () => {
     const response = await fetch(
-      `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,timestamp,permalink&access_token=${process.env.INSTAGRAM_TOKEN}...`
+      `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,timestamp,permalink&access_token=${process.env.REACT_APP_INSTAGRAM_API_KEY}...`
     );
     const data = await response.json();
     if (!response.ok) {
